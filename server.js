d07402cadf132c4e16d5ddfe40e11e5344f8e77b
Priyanshu Agarwal <109196864+priyanshu-101@users.noreply.github.com>
2024-10-02 19:23:03 +0530

    Update server.js

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.use(cors());
 
 // Set up MySQL connection
 const db = mysql.createConnection({
-  host: 'localhost',
+  host: '127.0.0.1',
   user: 'root',
   password: '@Priyanshu100',
   database: 'usercb'
